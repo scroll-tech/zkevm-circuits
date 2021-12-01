638b1c4279176d54e46bbbb886d098cc26c3c20f
HAOYUatHZ <haoyu@protonmail.com>
2021-12-01 20:46:47 +0800

    add evm::opcodes::pop::pop_tests::pop_opcode_impl

--- bus-mapping/src/evm/opcodes/pop.rs
+++ bus-mapping/src/evm/opcodes/pop.rs
@@ -28,3 +28,74 @@ impl Opcode for Pop {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod pop_tests {
+    use super::*;
+    use crate::{
+        bytecode,
+        circuit_input_builder::{
+            CircuitInputBuilder, ExecStep, Transaction, TransactionContext,
+        },
+        eth_types::Word,
+        evm::StackAddress,
+        mock,
+    };
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn pop_opcode_impl() -> Result<(), Error> {
+        let code = bytecode! {
+            PUSH1(0x80)
+            #[start]
+            POP
+            STOP
+        };
+
+        // Get the execution steps from the external tracer
+        let block =
+            mock::BlockData::new_single_tx_trace_code_at_start(&code).unwrap();
+
+        let mut builder = CircuitInputBuilder::new(
+            block.eth_block.clone(),
+            block.block_ctants.clone(),
+        );
+        builder.handle_tx(&block.eth_tx, &block.geth_trace).unwrap();
+
+        let mut test_builder = CircuitInputBuilder::new(
+            block.eth_block,
+            block.block_ctants.clone(),
+        );
+        let mut tx = Transaction::new(&block.eth_tx);
+        let mut tx_ctx = TransactionContext::new(&block.eth_tx);
+
+        // Generate step corresponding to SLOAD
+        let mut step = ExecStep::new(
+            &block.geth_trace.struct_logs[0],
+            0,
+            test_builder.block_ctx.gc,
+            0,
+        );
+        let mut state_ref =
+            test_builder.state_ref(&mut tx, &mut tx_ctx, &mut step);
+        // Add StackOp associated to the stack pop.
+        state_ref.push_op(StackOp::new(
+            RW::READ,
+            StackAddress::from(1023),
+            Word::from(0x80u32),
+        ));
+        tx.steps_mut().push(step);
+        test_builder.block.txs_mut().push(tx);
+
+        // Compare first step bus mapping instance
+        assert_eq!(
+            builder.block.txs()[0].steps()[0].bus_mapping_instance,
+            test_builder.block.txs()[0].steps()[0].bus_mapping_instance
+        );
+
+        // Compare containers
+        assert_eq!(builder.block.container, test_builder.block.container);
+
+        Ok(())
+    }
+}
