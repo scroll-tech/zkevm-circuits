3f88eb2150518be06c7cec2cb7f7cfec1c8e94a9
HAOYUatHZ <haoyu@protonmail.com>
2021-12-02 11:59:33 +0800

    fix

--- zkevm-circuits/src/evm_circuit/execution/pop.rs
+++ zkevm-circuits/src/evm_circuit/execution/pop.rs
@@ -81,20 +81,12 @@ impl<F: FieldExt> ExecutionGadget<F> for PopGadget<F> {
 mod test {
     use crate::evm_circuit::{
         bus_mapping_tmp_convert,
-        execution::bus_mapping_tmp::{
-            Block, Bytecode, Call, ExecStep, Rw, Transaction,
-        },
-        step::ExecutionResult,
         test::{rand_word, run_test_circuit_incomplete_fixed_table},
-        util::RandomLinearCombination,
     };
     use bus_mapping::{
         bytecode,
-        eth_types::{ToBigEndian, ToLittleEndian, Word},
-        evm::OpcodeId,
+        eth_types::{ToBigEndian, ToLittleEndian},
     };
-    use halo2::arithmetic::FieldExt;
-    use pasta_curves::pallas::Base;
 
     fn test_ok(value: Word) {
         let bytecode = bytecode! {
