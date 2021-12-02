b8b9ad87b65927033349836a1d2d748f707e6086
HAOYUatHZ <haoyu@protonmail.com>
2021-12-02 13:12:47 +0800

    fix

--- zkevm-circuits/src/evm_circuit/execution/dup.rs
+++ zkevm-circuits/src/evm_circuit/execution/dup.rs
@@ -87,23 +87,9 @@ impl<F: FieldExt> ExecutionGadget<F> for DupGadget<F> {
 mod test {
     use crate::evm_circuit::{
         bus_mapping_tmp_convert,
-        execution::{
-            bus_mapping_tmp::{
-                Block, Bytecode, Call, ExecStep, Rw, Transaction,
-            },
-            byte,
-        },
-        step::ExecutionResult,
         test::{rand_word, run_test_circuit_incomplete_fixed_table},
-        util::RandomLinearCombination,
     };
-    use bus_mapping::{
-        bytecode,
-        eth_types::{ToBigEndian, ToLittleEndian, Word},
-        evm::OpcodeId,
-    };
-    use halo2::arithmetic::FieldExt;
-    use pasta_curves::pallas::Base;
+    use bus_mapping::{bytecode, eth_types::Word, evm::OpcodeId};
 
     fn test_ok(opcode: OpcodeId, value: Word) {
         let n = (opcode.as_u8() - OpcodeId::DUP1.as_u8() + 1) as usize;
