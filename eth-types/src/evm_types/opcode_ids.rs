28c442ce129a119e8898d103ba9eaa2139eb9849
Rohit Narurkar <rohit.narurkar@protonmail.com>
2022-01-17 16:38:43 +0800

    fix: bus mapping impl and refactoring

--- eth-types/src/evm_types/opcode_ids.rs
+++ eth-types/src/evm_types/opcode_ids.rs
@@ -501,7 +501,7 @@ impl OpcodeId {
             OpcodeId::SMOD => GasCost::FAST,
             OpcodeId::ADDMOD => GasCost::MID,
             OpcodeId::MULMOD => GasCost::MID,
-            OpcodeId::EXP => GasCost::ZERO,
+            OpcodeId::EXP => GasCost::SLOW,
             OpcodeId::SIGNEXTEND => GasCost::FAST,
             OpcodeId::LT => GasCost::FASTEST,
             OpcodeId::GT => GasCost::FASTEST,
