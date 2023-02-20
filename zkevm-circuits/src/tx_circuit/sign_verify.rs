7a8b263f0bd89142e314b308fe8f106c6fae735c
zhenfei <zhenfei.zhang@hotmail.com>
2023-02-20 09:22:51 -0500

    clean up

--- zkevm-circuits/src/tx_circuit/sign_verify.rs
+++ zkevm-circuits/src/tx_circuit/sign_verify.rs
@@ -384,14 +384,11 @@ impl<F: Field> SignVerifyChip<F> {
 
         // address is the random linear combination of the public key
         // it is fine to use a phase 1 gate here
-
         let (_pk, _, address) = ecdsa_chip.range.gate.inner_product(
             ctx,
             &powers_of_256_cells[..20].to_vec(),
             &pk_hash_cells[..20].to_vec(),
         )?;
-
-        println!("address in circuit {:?}", address);
 
         let is_address_zero = ecdsa_chip.range.is_equal(
             ctx,
