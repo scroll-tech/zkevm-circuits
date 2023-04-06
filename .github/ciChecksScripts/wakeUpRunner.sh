d924624d3622012f32c1898eaa0e247294771348
Steven Gu <asongala@163.com>
2023-04-06 23:40:14 +0800

    Update

--- .github/ciChecksScripts/wakeUpRunner.sh
+++ .github/ciChecksScripts/wakeUpRunner.sh
@@ -6,8 +6,8 @@ runner_vpc_id2="vpc-1176d875"
 region_opt="--region us-west-2"
 
 # Get runner status
-runner1=$(aws ec2 describe-instances --profile $profile --filters Name=tag:Name,Values=[jenkins1] Name=network-interface.vpc-id,Values=[$runner_vpc_id1] --query "Reservations[*].Instances[*][InstanceId]" ${region_opt} --output text | xargs)
-runner2=$(aws ec2 describe-instances --profile $profile --filters Name=tag:Name,Values=[jenkins2] Name=network-interface.vpc-id,Values=[$runner_vpc_id2] --query "Reservations[*].Instances[*][InstanceId]" --output text | xargs)
+runner1=$(aws ec2 describe-instances --profile $profile --filters Name=tag:Name,Values=[jenkins] Name=network-interface.vpc-id,Values=[$runner_vpc_id1] --query "Reservations[*].Instances[*][InstanceId]" ${region_opt} --output text | xargs)
+runner2=$(aws ec2 describe-instances --profile $profile --filters Name=tag:Name,Values=[jenkins] Name=network-interface.vpc-id,Values=[$runner_vpc_id2] --query "Reservations[*].Instances[*][InstanceId]" --output text | xargs)
 
 while true; do
     runner_status=$(aws ec2 describe-instances --profile $profile --instance-ids $runner1 --query "Reservations[*].Instances[*].State.[Name]" ${region_opt} --output text)
