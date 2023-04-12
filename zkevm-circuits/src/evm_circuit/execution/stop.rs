cabda151546786b929bccfbb4b48c4b100361c40
Zhang Zhuo <mycinbrin@gmail.com>
2023-04-12 11:13:16 +0800

    Update stop.rs

--- zkevm-circuits/src/evm_circuit/execution/stop.rs
+++ zkevm-circuits/src/evm_circuit/execution/stop.rs
@@ -40,9 +40,9 @@ impl<F: Field> ExecutionGadget<F> for StopGadget<F> {
         let is_out_of_range =
             LtGadget::construct(cb, cb.curr.state.program_counter.expr(), code_length.expr());
         let opcode = cb.query_cell();
-        // cb.condition(1.expr() - is_out_of_range.expr(), |cb| {
-        // cb.opcode_lookup(opcode.expr(), 1.expr());
-        // });
+        cb.condition(1.expr() - is_out_of_range.expr(), |cb| {
+            cb.opcode_lookup(opcode.expr(), 1.expr());
+        });
 
         // We do the responsible opcode check explicitly here because we're not using
         // the `SameContextGadget` for `STOP`.
