feb93e283330e72f3409ad80cb11fac2638cfaf2
Zhang Zhuo <mycinbrin@gmail.com>
2023-05-15 16:53:00 +0800

    fix copy rows

--- zkevm-circuits/src/test_util.rs
+++ zkevm-circuits/src/test_util.rs
@@ -1,7 +1,7 @@
 //! Testing utilities
 
 use crate::{
-    copy_circuit::{CopyCircuit, ExternalData},
+    copy_circuit::CopyCircuit,
     evm_circuit::EvmCircuit,
     state_circuit::StateCircuit,
     util::{log2_ceil, SubCircuit},
@@ -249,8 +249,10 @@ impl<const NACC: usize, const NTX: usize> CircuitTestBuilder<NACC, NTX> {
 
         // Run copy circuit test
         {
-            let (active_rows, _max_rows) = CopyCircuit::<Fr>::min_num_rows_block(&block);
-            let k = block.get_test_degree();
+            let (active_rows, max_rows) = CopyCircuit::<Fr>::min_num_rows_block(&block);
+            let k1 = block.get_test_degree();
+            let k2 = log2_ceil(max_rows + NUM_BLINDING_ROWS);
+            let k = k1.max(k2);
             let copy_circuit = CopyCircuit::<Fr>::new_from_block(&block);
             let instance = copy_circuit.instance();
             let prover = MockProver::<Fr>::run(k, &copy_circuit, instance).unwrap();
