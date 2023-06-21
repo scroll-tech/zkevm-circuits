a3f4aef066c148fe651427e64340801454221c5f
zhenfei <zhenfei.zhang@hotmail.com>
2023-06-20 20:24:35 -0400

    [fix] typo in scripts

--- aggregator/tests.sh
+++ aggregator/tests.sh
@@ -2,9 +2,8 @@ RUST_LOG=trace MODE=greeter cargo test --release --features=print-trace test_pi_
 RUST_LOG=trace MODE=greeter cargo test --release --features=print-trace test_pi_aggregation_real_prover -- --nocapture 2>&1 | tee pi_real.log
 RUST_LOG=trace MODE=greeter cargo test --release --features=print-trace test_mock_chunk_prover -- --nocapture 2>&1 | tee mock_chunk.log
 
-
-# the following 3 tests takes super long time
-# RUST_LOG=trace MODE=greeter cargo test --release --features=print-trace test_proof_compression -- --nocapture 2>&1 | tee mock_chunk.log
+# the following 4 tests takes super long time
+# RUST_LOG=trace MODE=greeter cargo test --release --features=print-trace test_proof_compression -- --nocapture 2>&1 | tee compression.log
 # RUST_LOG=trace MODE=greeter cargo test --release --features=print-trace test_two_layer_proof_compression -- --ignored --nocapture 2>&1 | tee compression2.log
 # RUST_LOG=trace MODE=greeter cargo test --release --features=print-trace test_aggregation_circuit -- --ignored --nocapture 2>&1 | tee aggregation.log
 # RUST_LOG=trace MODE=greeter cargo test --release --features=print-trace test_e2e -- --ignored --nocapture 2>&1 | tee aggregation.log
