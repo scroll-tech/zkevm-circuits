85d4f39c15b9a5d16e16015c1eb505e9a6488b49
Zhang Zhuo <mycinbrin@gmail.com>
2023-06-27 15:39:05 +0800

    fix

--- zkevm-circuits/src/pi_circuit.rs
+++ zkevm-circuits/src/pi_circuit.rs
@@ -8,6 +8,7 @@ mod param;
 mod test;
 
 use std::{iter, marker::PhantomData};
+use std::str::FromStr;
 
 use crate::{evm_circuit::util::constraint_builder::ConstrainBuilderCommon, table::KeccakTable};
 use bus_mapping::circuit_input_builder::get_dummy_tx_hash;
