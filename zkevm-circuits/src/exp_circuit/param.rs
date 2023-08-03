633ccb3897ecac3ae5deafaaf0f1b1ca8fde198e
lightsing <light.tsing@gmail.com>
2023-08-03 15:25:51 +0800

    fix exp_circuit

--- zkevm-circuits/src/exp_circuit/param.rs
+++ zkevm-circuits/src/exp_circuit/param.rs
@@ -1,5 +1,5 @@
 /// The number of rows assigned for each step in an exponentiation trace.
-pub(crate) const OFFSET_INCREMENT: usize = 7usize;
+pub(crate) const OFFSET_INCREMENT: usize = 8usize;
 /// The number of rows required for the exponentiation table within the circuit
 /// for each step.
 pub(crate) const ROWS_PER_STEP: usize = 4usize;
