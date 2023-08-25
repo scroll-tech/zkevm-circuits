747e4f977a0005bc91e923ca3d7e1378fb9e0ae6
Rohit Narurkar <rohit.narurkar@protonmail.com>
2023-08-25 16:33:15 +0100

    chore: refactor unnecessary cell being returned

--- zkevm-circuits/src/evm_circuit/util/constraint_builder.rs
+++ zkevm-circuits/src/evm_circuit/util/constraint_builder.rs
@@ -331,7 +331,7 @@ impl<'a, F: Field> ConstrainBuilderCommon<F> for EVMConstraintBuilder<'a, F> {
     }
 }
 
-pub(crate) type BoxedClosure<'a, F, R> = Box<dyn FnOnce(&mut EVMConstraintBuilder<F>) -> R + 'a>;
+pub(crate) type BoxedClosure<'a, F> = Box<dyn FnOnce(&mut EVMConstraintBuilder<F>) + 'a>;
 
 impl<'a, F: Field> EVMConstraintBuilder<'a, F> {
     pub(crate) fn new(
@@ -1530,12 +1530,12 @@ impl<'a, F: Field> EVMConstraintBuilder<'a, F> {
     /// used for constraining the internal states for precompile calls. Each precompile call
     /// expects a different cell layout, but since the next state can be at the most one precompile
     /// state, we can re-use cells assigned across all those conditions.
-    pub(crate) fn constrain_mutually_exclusive_next_step<R: Expr<F>>(
+    pub(crate) fn constrain_mutually_exclusive_next_step(
         &mut self,
         conditions: Vec<Expression<F>>,
         next_states: Vec<ExecutionState>,
-        constraints: Vec<BoxedClosure<F, R>>,
-    ) -> Expression<F> {
+        constraints: Vec<BoxedClosure<F>>,
+    ) {
         assert_eq!(conditions.len(), constraints.len());
         assert_eq!(conditions.len(), next_states.len());
         self.require_boolean(
@@ -1546,16 +1546,14 @@ impl<'a, F: Field> EVMConstraintBuilder<'a, F> {
         // record the heights of all columns (for the next step) as we begin cell assignment.
         let start_heights = self.next.cell_manager.get_heights();
 
-        let mut conditional_outputs: Vec<R> = Vec::with_capacity(constraints.len());
         let mut max_end_heights: Vec<usize> = vec![0usize; start_heights.len()];
         for ((&next_state, condition), constraint) in next_states
             .iter()
-            .zip(conditions.clone().into_iter())
+            .zip(conditions.into_iter())
             .zip(constraints.into_iter())
         {
             // constraint the next step.
-            let r = self.constrain_next_step(next_state, Some(condition), constraint);
-            conditional_outputs.push(r);
+            self.constrain_next_step(next_state, Some(condition), constraint);
 
             // get the column heights at the end of querying cells in the above constraints.
             let end_heights = self.next.cell_manager.get_heights();
@@ -1572,14 +1570,6 @@ impl<'a, F: Field> EVMConstraintBuilder<'a, F> {
 
         // reset height of next step to the maximum heights of each column.
         self.next.cell_manager.reset_heights_to(&max_end_heights);
-
-        sum::expr(
-            conditions
-                .into_iter()
-                .zip(conditional_outputs.iter())
-                .map(|(cond, r)| cond * r.expr())
-                .collect::<Vec<Expression<F>>>(),
-        )
     }
 
     /// This function needs to be used with extra precaution. You need to make
