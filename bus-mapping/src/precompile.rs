a999ea43b2baa5bac921aa7fb965a2796e3f3611
Zhuo Zhang <mycinbrin@gmail.com>
2023-08-31 07:03:45 +0000

    fix

--- bus-mapping/src/precompile.rs
+++ bus-mapping/src/precompile.rs
@@ -29,20 +29,24 @@ pub(crate) fn execute_precompiled(
     );
     let (return_data, gas_cost, is_oog, is_ok) = match precompile_fn(input, gas) {
         Ok((gas_cost, return_value)) => {
-            match PrecompileCalls::from(address.0[19]) {
-                // Revm behavior is different from scroll evm,
-                // so we need to override the behavior of invalid input
-                PrecompileCalls::Modexp => {
-                    let (input_valid, [_, _, modulus_len]) = ModExpAuxData::check_input(input);
-                    if input_valid {
-                        // detect some edge cases like modulus = 0
-                        assert_eq!(modulus_len.as_usize(), return_value.len());
-                        (return_value, gas_cost, false, true) // no oog error
-                    } else {
-                        (vec![], gas, false, false)
+            if cfg!(feature = "scroll") {
+                match PrecompileCalls::from(address.0[19]) {
+                    // Revm behavior is different from scroll evm,
+                    // so we need to override the behavior of invalid input
+                    PrecompileCalls::Modexp => {
+                        let (input_valid, [_, _, modulus_len]) = ModExpAuxData::check_input(input);
+                        if input_valid {
+                            // detect some edge cases like modulus = 0
+                            assert_eq!(modulus_len.as_usize(), return_value.len());
+                            (return_value, gas_cost, false, true) // no oog error
+                        } else {
+                            (vec![], gas, false, false)
+                        }
                     }
+                    _ => (return_value, gas_cost, false, true),
                 }
-                _ => (return_value, gas_cost, false, true),
+            } else {
+                (return_value, gas_cost, false, true)
             }
         }
         Err(err) => match err {
