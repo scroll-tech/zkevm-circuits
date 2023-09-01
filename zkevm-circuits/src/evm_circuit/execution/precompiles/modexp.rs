24eba44e4cf550ad014b12f16192070215c9cd20
Dream Wu <wwuwwei@126.com>
2023-09-01 15:39:54 +0800

    test input len < 96

--- zkevm-circuits/src/evm_circuit/execution/precompiles/modexp.rs
+++ zkevm-circuits/src/evm_circuit/execution/precompiles/modexp.rs
@@ -1098,7 +1098,7 @@ mod test {
                     ..Default::default()
                 },
                 PrecompileCallArgs {
-                    name: "modexp success with padding 0",
+                    name: "modexp success with padding 0, input len > minimal 96 bytes",
                     setup_code: bytecode! {
                         // Base size
                         PUSH1(0x1)
@@ -1119,6 +1119,33 @@ mod test {
                     },
                     call_data_offset: 0x0.into(),
                     call_data_length: 0x65.into(),
+                    ret_offset: 0x9f.into(),
+                    ret_size: 0x01.into(),
+                    address: PrecompileCalls::Modexp.address().to_word(),
+                    ..Default::default()
+                },
+                PrecompileCallArgs {
+                    name: "modexp success with padding 0, input len < minimal 96 bytes",
+                    setup_code: bytecode! {
+                        // Base size
+                        PUSH1(0x1)
+                        PUSH1(0x00)
+                        MSTORE
+                        // Esize
+                        PUSH1(0x3)
+                        PUSH1(0x20)
+                        MSTORE
+                        // Msize
+                        PUSH1(0x2)
+                        PUSH1(0x40)
+                        MSTORE
+                        // B, E and M
+                        PUSH32(word!("0x0800000901000000000000000000000000000000000000000000000000000000"))
+                        PUSH1(0x60)
+                        MSTORE
+                    },
+                    call_data_offset: 0x0.into(),
+                    call_data_length: 0x40.into(), // < minimal 96 bytes
                     ret_offset: 0x9f.into(),
                     ret_size: 0x01.into(),
                     address: PrecompileCalls::Modexp.address().to_word(),
