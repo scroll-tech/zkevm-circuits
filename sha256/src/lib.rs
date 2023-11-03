215a1a20f58d53234079ad9fecdfc235d0ad5880
Ho Vei <noelwei@gmail.com>
2023-11-03 21:32:30 +0800

    add required cfgs

--- sha256/src/lib.rs
+++ sha256/src/lib.rs
@@ -1,4 +1,14 @@
-// sha256 adapt table16 in halo2_gadgets
+//! sha256 adapt table16 in halo2_gadgets
+
+#![cfg_attr(docsrs, feature(doc_cfg))]
+// Temporary until we have more of the crate implemented.
+#![allow(dead_code)]
+// Catch documentation errors caused by code changes.
+#![deny(rustdoc::broken_intra_doc_links)]
+#![deny(missing_debug_implementations)]
+#![deny(missing_docs)]
+#![deny(unsafe_code)]
+
 
 // The size of a SHA-256 digest, in 32-bit words.
 const DIGEST_SIZE: usize = 8;
