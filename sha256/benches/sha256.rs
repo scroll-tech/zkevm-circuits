c1ea6b87b5ab69a44a230bbc612930caa0b94480
Ho Vei <noelwei@gmail.com>
2023-11-10 16:25:49 +0800

    customable bench

--- sha256/benches/sha256.rs
+++ sha256/benches/sha256.rs
@@ -15,9 +15,13 @@ use std::{
 
 use criterion::{criterion_group, criterion_main, Criterion};
 
-use sha256::{table16::{BlockWord, Table16Chip, Table16Config}, Sha256, BLOCK_SIZE};
+use sha256::{
+    table16::{BlockWord, Table16Chip, Table16Config},
+    Sha256, BLOCK_SIZE,
+};
 
 use halo2_proofs::{
+    halo2curves::bn256::{Bn256, Fr},
     poly::{
         commitment::ParamsProver,
         kzg::{
@@ -26,10 +30,10 @@ use halo2_proofs::{
             strategy::SingleStrategy,
         },
     },
-    halo2curves::bn256::{Bn256, Fq, Fr, G1Affine},
     transcript::{TranscriptReadBuffer, TranscriptWriterBuffer},
 };
 
+const CAP_BLK: usize = 64;
 
 #[allow(dead_code)]
 fn bench(name: &str, k: u32, c: &mut Criterion) {
@@ -77,8 +81,8 @@ fn bench(name: &str, k: u32, c: &mut Criterion) {
             ];
 
             // Create a message of length 31 blocks
-            let mut input = Vec::with_capacity(31 * BLOCK_SIZE);
-            for _ in 0..31 {
+            let mut input = Vec::with_capacity(CAP_BLK * BLOCK_SIZE);
+            for _ in 0..CAP_BLK {
                 input.extend_from_slice(&test_input);
             }
 
@@ -89,14 +93,16 @@ fn bench(name: &str, k: u32, c: &mut Criterion) {
     }
 
     // Initialize the polynomial commitment parameters
-    let params_path = Path::new("./benches/sha256_assets/sha256_params");
+    let path_str = format!("./benches/sha256_assets/sha256_params_{}", k);
+    let params_path = Path::new(&path_str);
     if File::open(&params_path).is_err() {
         let params: ParamsKZG<Bn256> = ParamsKZG::new(k);
         let mut buf = Vec::new();
 
         params.write(&mut buf).expect("Failed to write params");
 
-        create_dir("./benches/sha256_assets").unwrap_or_else(|_| println!("Params dir already exists"));
+        create_dir("./benches/sha256_assets")
+            .unwrap_or_else(|_| println!("Params dir already exists"));
         let mut file = File::create(&params_path).expect("Failed to create sha256_params");
 
         file.write_all(&buf[..])
@@ -115,20 +121,20 @@ fn bench(name: &str, k: u32, c: &mut Criterion) {
 
     let circuit: MyCircuit = MyCircuit {};
 
-    let prover_name = name.to_string() + "-prover";
-    let verifier_name = name.to_string() + "-verifier";
+    let prover_name = format!("{}-{}-bytes-prover", name.to_string(), CAP_BLK * 64);
+    let verifier_name = format!("{}-{}-bytes-verifier", name.to_string(), CAP_BLK * 64);
 
     // Benchmark proof creation
     c.bench_function(&prover_name, |b| {
         b.iter(|| {
             let mut transcript = Blake2bWrite::<_, _, Challenge255<_>>::init(vec![]);
-            create_proof::<KZGCommitmentScheme<Bn256>,  ProverSHPLONK<_>, _, _, _, _>(
+            create_proof::<KZGCommitmentScheme<Bn256>, ProverSHPLONK<_>, _, _, _, _>(
                 &params,
-                &pk, 
+                &pk,
                 &[circuit],
                 &[],
                 OsRng,
-                &mut transcript
+                &mut transcript,
             )
             .expect("proof generation should not fail");
             let _proof: Vec<u8> = transcript.finalize();
@@ -136,7 +142,8 @@ fn bench(name: &str, k: u32, c: &mut Criterion) {
     });
 
     // Create a proof
-    let proof_path = Path::new("./benches/sha256_assets/sha256_proof");
+    let path_str = format!("./benches/sha256_assets/sha256_proof_{}", k);
+    let proof_path = Path::new(&path_str);
     if File::open(&proof_path).is_err() {
         let mut transcript = Blake2bWrite::<_, _, Challenge255<_>>::init(vec![]);
         create_proof::<KZGCommitmentScheme<Bn256>, ProverSHPLONK<_>, _, _, _, _>(
@@ -163,14 +170,15 @@ fn bench(name: &str, k: u32, c: &mut Criterion) {
         b.iter(|| {
             let strategy = SingleStrategy::new(&params);
             let mut transcript = Blake2bRead::<_, _, Challenge255<_>>::init(&proof[..]);
-            let _strategy = verify_proof::<KZGCommitmentScheme<Bn256>, VerifierSHPLONK<_>, _, _, _>(
-                &params,
-                pk.get_vk(),
-                strategy,
-                &[],
-                &mut transcript,
-            )
-            .unwrap();
+            let _strategy =
+                verify_proof::<KZGCommitmentScheme<Bn256>, VerifierSHPLONK<_>, _, _, _>(
+                    &params,
+                    pk.get_vk(),
+                    strategy,
+                    &[],
+                    &mut transcript,
+                )
+                .unwrap();
         });
     });
 }
@@ -181,7 +189,7 @@ fn criterion_benchmark(c: &mut Criterion) {
     // bench("sha256", 20, c);
 }
 
-criterion_group!{
+criterion_group! {
     name = benches;
     config = Criterion::default().sample_size(20);
     targets = criterion_benchmark
