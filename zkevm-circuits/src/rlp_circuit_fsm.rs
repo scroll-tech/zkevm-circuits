a548910bac59623879cf198ee8e532b0aa544f79
darth-cy <qg2153@columbia.edu>
2023-11-19 20:13:50 -0800

    remove debug flag

--- zkevm-circuits/src/rlp_circuit_fsm.rs
+++ zkevm-circuits/src/rlp_circuit_fsm.rs
@@ -1578,34 +1578,6 @@ impl<F: Field> RlpCircuitConfig<F> {
         // hence using depth alone is sufficient to determine clearing conditions.
         // however, this might change in the future if more nested structures are introduced at same
         // depth level
-        // 1030_debug
-        // meta.create_gate(
-        //     "access list: clearing access_list_idx and storage_key_idx",
-        //     |meta| {
-        //         let mut cb = BaseConstraintBuilder::default();
-
-        //         cb.condition(depth_eq_two.is_equal_expression.expr(), |cb| {
-        //             cb.require_equal(
-        //                 "al_idx = 0",
-        //                 meta.query_advice(rlp_table.access_list_idx, Rotation::cur()),
-        //                 0.expr(),
-        //             );
-        //         });
-
-        //         cb.condition(depth_eq_four.is_equal_expression.expr(), |cb| {
-        //             cb.require_equal(
-        //                 "sk_idx = 0",
-        //                 meta.query_advice(rlp_table.storage_key_idx, Rotation::cur()),
-        //                 0.expr(),
-        //             );
-        //         });
-
-        //         cb.gate(and::expr([
-        //             meta.query_fixed(q_enabled, Rotation::cur()),
-        //             is_tag_end_vector(meta),
-        //         ]))
-        //     },
-        // );
         meta.create_gate(
             "access list: clearing access_list_idx and storage_key_idx",
             |meta| {
