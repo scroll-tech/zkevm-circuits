ba9fa62ceb47d009bb1db206cf3510b32d5f9121
darth-cy <qg2153@columbia.edu>
2023-12-04 16:48:03 -0800

    remove debug tag

--- zkevm-circuits/src/tx_circuit/test.rs
+++ zkevm-circuits/src/tx_circuit/test.rs
@@ -186,8 +186,7 @@ fn run<F: Field>(
 }
 
 #[test]
-// tx1559_debug_new_test
-// #[cfg(feature = "scroll")]
+#[cfg(feature = "scroll")]
 fn tx_circuit_1tx_2max_eip1559() {
     const MAX_TXS: usize = 2;
     const MAX_CALLDATA: usize = 3200;
