f523352231711143e63d4f12ec6fd0fa407b111a
darth-cy <qg2153@columbia.edu>
2024-01-06 22:44:15 -0500

    add op constraints

--- zkevm-circuits/src/rlp_circuit_fsm.rs
+++ zkevm-circuits/src/rlp_circuit_fsm.rs
@@ -1719,6 +1719,7 @@ impl<F: Field> RlpCircuitConfig<F> {
                 meta.query_advice(rlp_decoding_table.is_stack_update, Rotation::cur()),
             );
 
+            // Operation-specific Constraints
             // Stack Init
             cb.condition(
                 meta.query_advice(rlp_decoding_table.is_stack_init, Rotation::cur()),
@@ -1740,63 +1741,61 @@ impl<F: Field> RlpCircuitConfig<F> {
                 },
             );
 
-            // tx1559_debug
-            // Stack depth must stay same or only increment by 1 when the stack is not initiating
-            // cb.condition(
-            //     not::expr(meta.query_advice(rlp_decoding_table.is_stack_init, Rotation::cur())),
-            //     |cb| {
-            //         cb.require_zero(
-            //             "stack depth stays the same or increments by 1",
-            //             (meta.query_advice(rlp_decoding_table.depth, Rotation::cur())
-            //                 - meta.query_advice(rlp_decoding_table.depth, Rotation::prev()))
-            //                 * (meta.query_advice(rlp_decoding_table.depth, Rotation::cur())
-            //                     - meta.query_advice(rlp_decoding_table.depth, Rotation::prev())
-            //                     - 1.expr()),
-            //         );
-            //     },
-            // );
-
-            // tx1559_debug
             // Stack Push
-            // cb.condition(
-            //     meta.query_advice(rlp_decoding_table.is_stack_push, Rotation::cur()),
-            //     |cb| {
-            //         cb.require_zero(
-            //             "The higher depth must have 0 bytes.",
-            //             meta.query_advice(rlp_decoding_table.value_prev, Rotation::cur()),
-            //         );
-            //         cb.require_zero(
-            //             "Before a new PUSH, all previous bytes must be processed.",
-            //             meta.query_advice(rlp_decoding_table.value, Rotation::prev()),
-            //         );
-            //     },
-            // );
+            cb.condition(
+                meta.query_advice(rlp_decoding_table.is_stack_push, Rotation::cur()),
+                |cb| {
+                    cb.require_zero(
+                        "Before a new PUSH, all previous bytes must be processed.",
+                        meta.query_advice(rlp_decoding_table.value, Rotation::prev()),
+                    );
+                    cb.require_zero(
+                        "PUSH has value_prev at 0",
+                        meta.query_advice(rlp_decoding_table.value_prev, Rotation::cur()),
+                    );
+                },
+            );
 
-            // tx1559_debug
+            // Stack Pop
+            cb.condition(
+                meta.query_advice(rlp_decoding_table.is_stack_pop, Rotation::cur()),
+                |cb| {
+                    cb.require_equal(
+                        "POP stack operation doesn't skip bytes",
+                        meta.query_advice(rlp_decoding_table.value, Rotation::prev()),
+                        meta.query_advice(rlp_decoding_table.value_prev, Rotation::cur()),
+                    );
+                },
+            );
+
+            // Stack Update
+            cb.condition(
+                meta.query_advice(rlp_decoding_table.is_stack_update, Rotation::cur()),
+                |cb| {
+                    cb.require_equal(
+                        "UPDATE doesn't change depth",
+                        meta.query_advice(rlp_decoding_table.depth, Rotation::prev()),
+                        meta.query_advice(rlp_decoding_table.depth, Rotation::cur()),
+                    );
+                    cb.require_equal(
+                        "UPDATE stack operation reads 1 byte",
+                        meta.query_advice(rlp_decoding_table.value, Rotation::cur()) + 1.expr(),
+                        meta.query_advice(rlp_decoding_table.value_prev, Rotation::cur()),
+                    );
+                    cb.require_equal(
+                        "UPDATE stack operation doesn't skip bytes",
+                        meta.query_advice(rlp_decoding_table.value, Rotation::prev()),
+                        meta.query_advice(rlp_decoding_table.value_prev, Rotation::cur()),
+                    );
+                },
+            );
+
             cb.condition(
                 stack_op_id_check.is_equal_expression.expr(),
                 |cb| {
                     cb.require_zero(
                         "When key stays the same, operation can only be UPDATE or POP",
                         meta.query_advice(rlp_decoding_table.is_stack_push, Rotation::next()),
-                    );
-                },
-            );
-            cb.condition(
-                and::expr([
-                    stack_op_id_check.is_equal_expression.expr(),
-                    meta.query_advice(rlp_decoding_table.is_stack_update, Rotation::next()),
-                ]),
-                |cb| {
-                    cb.require_equal(
-                        "UPDATE stack operation reads 1 byte",
-                        meta.query_advice(rlp_decoding_table.value, Rotation::next()) + 1.expr(),
-                        meta.query_advice(rlp_decoding_table.value_prev, Rotation::next()),
-                    );
-                    cb.require_equal(
-                        "UPDATE stack operation doesn't skip bytes",
-                        meta.query_advice(rlp_decoding_table.value, Rotation::cur()),
-                        meta.query_advice(rlp_decoding_table.value_prev, Rotation::next()),
                     );
                 },
             );
