95086e12dc3d1fa343886d89aec1082876bfa183
Vladimir Trifonov <trifonov.vp@gmail.com>
2024-01-15 11:34:59 +0200

    fix: add export ChunkTrace

--- prover/src/lib.rs
+++ prover/src/lib.rs
@@ -14,7 +14,7 @@ pub mod utils;
 pub mod zkevm;
 
 pub use common::{ChunkHash, CompressionCircuit};
-pub use eth_types::l2_types::BlockTrace;
+pub use eth_types::l2_types::{BlockTrace, ChunkTrace};
 pub use proof::{BatchProof, ChunkProof, EvmProof, Proof};
 pub use snark_verifier_sdk::{CircuitExt, Snark};
 pub use types::WitnessBlock;
