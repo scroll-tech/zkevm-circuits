7332d9ee3561c2925e56b973cada80e16ec15b2a
张三 <32984960+johnsonjie@users.noreply.github.com>
2024-01-31 17:15:03 +0800

    Update wakeUpRunner.sh

--- .github/ciChecksScripts/wakeUpRunner.sh
+++ .github/ciChecksScripts/wakeUpRunner.sh
@@ -8,7 +8,7 @@ region_opt="--region us-west-2"
 runner1=$(aws ec2 describe-instances --profile $profile --filters Name=tag:Name,Values=[super1] Name=network-interface.vpc-id,Values=[$runner_vpc_id] --query "Reservations[*].Instances[*][InstanceId]" ${region_opt} --output text | xargs)
 runner2=$(aws ec2 describe-instances --profile $profile --filters Name=tag:Name,Values=[super2] Name=network-interface.vpc-id,Values=[$runner_vpc_id] --query "Reservations[*].Instances[*][InstanceId]" ${region_opt} --output text | xargs)
 
-
+# start runner1
 while true; do
     runner_status=$(aws ec2 describe-instances --profile $profile --instance-ids $runner1 --query "Reservations[*].Instances[*].State.[Name]" ${region_opt} --output text)
     echo 'runner1 - '$runner_status
@@ -27,6 +27,7 @@ while true; do
     fi
 done
 
+# start runner2
 while true; do
     runner_status=$(aws ec2 describe-instances --profile $profile --instance-ids $runner2 --query "Reservations[*].Instances[*].State.[Name]" ${region_opt} --output text)
     echo 'runner2 - '$runner_status
