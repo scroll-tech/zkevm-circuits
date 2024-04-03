a23fb681c9cf30463ee02147c02377c977e0a9b8
Dream Wu <wwuwwei@126.com>
2024-04-03 17:25:18 +0800

    fix 2930 signature

--- zkevm-circuits/src/evm_circuit/util/common_gadget/tx_access_list.rs
+++ zkevm-circuits/src/evm_circuit/util/common_gadget/tx_access_list.rs
@@ -153,3 +153,89 @@ impl<F: Field> TxAccessListGadget<F> {
         address_len + storage_key_len
     }
 }
+
+
+// tests for eip2930
+#[cfg(test)]
+mod test {
+    use crate::test_util::CircuitTestBuilder;
+    use eth_types::{Error, Word};
+    use ethers_signers::Signer;
+    use mock::{eth, gwei, TestContext, MOCK_ACCOUNTS, MOCK_WALLETS};
+
+    #[test]
+    fn test_eip2930_tx_for_empty_access_list() {
+        let ctx = build_ctx(gwei(80_000), gwei(2), gwei(2)).unwrap();
+        CircuitTestBuilder::new_from_test_ctx(ctx).run();
+    }
+
+    // TODO: need to enable for scroll feature after merging this PR
+    // <https://github.com/scroll-tech/go-ethereum/pull/578>.
+    #[cfg(not(feature = "scroll"))]
+    #[test]
+    fn test_eip1559_tx_for_less_balance() {
+        let res = build_ctx(gwei(79_999), gwei(2), gwei(2));
+
+        // Return a tracing error if insufficient sender balance.
+        if let Error::TracingError(err) = res.unwrap_err() {
+            assert_eq!(err, "Failed to run Trace, err: Failed to apply config.Transactions[0]: insufficient funds for gas * price + value: address 0xEeFca179F40D3B8b3D941E6A13e48835a3aF8241 have 79999000000000 want 80000000000000");
+        } else {
+            panic!("Must be a tracing error");
+        }
+    }
+
+    #[test]
+    fn test_eip1559_tx_for_more_balance() {
+        let ctx = build_ctx(gwei(80_001), gwei(2), gwei(2)).unwrap();
+        CircuitTestBuilder::new_from_test_ctx(ctx).run();
+    }
+
+    #[test]
+    fn test_eip1559_tx_for_gas_fee_cap_gt_gas_tip_cap() {
+        // Should be successful if `max_fee_per_gas > max_priority_fee_per_gas`.
+        let ctx = build_ctx(gwei(80_000), gwei(2), gwei(1)).unwrap();
+
+        CircuitTestBuilder::new_from_test_ctx(ctx).run();
+    }
+
+    // TODO: need to enable for scroll feature after merging this PR
+    // <https://github.com/scroll-tech/go-ethereum/pull/578>.
+    #[cfg(not(feature = "scroll"))]
+    #[test]
+    fn test_eip1559_tx_for_gas_fee_cap_lt_gas_tip_cap() {
+        let res = build_ctx(gwei(80_000), gwei(1), gwei(2));
+
+        // Return a tracing error if `max_fee_per_gas < max_priority_fee_per_gas`.
+        if let Error::TracingError(err) = res.unwrap_err() {
+            assert_eq!(err, "Failed to run Trace, err: Failed to apply config.Transactions[0]: max priority fee per gas higher than max fee per gas: address 0xEeFca179F40D3B8b3D941E6A13e48835a3aF8241, maxPriorityFeePerGas: 2000000000, maxFeePerGas: 1000000000");
+        } else {
+            panic!("Must be a tracing error");
+        }
+    }
+
+    fn build_ctx(
+        sender_balance: Word,
+        max_fee_per_gas: Word,
+        max_priority_fee_per_gas: Word,
+    ) -> Result<TestContext<2, 1>, Error> {
+        TestContext::new(
+            None,
+            |accs| {
+                accs[0]
+                    .address(MOCK_WALLETS[0].address())
+                    .balance(sender_balance);
+                accs[1].address(MOCK_ACCOUNTS[0]).balance(eth(1));
+            },
+            |mut txs, _accs| {
+                txs[0]
+                    .from(MOCK_WALLETS[0].clone())
+                    .to(MOCK_ACCOUNTS[0])
+                    .gas(30_000.into())
+                    .gas_price(30_000.into())
+                    .value(gwei(20_000))
+                    .transaction_type(1); // Set tx type to EIP-2930.
+            },
+            |block, _tx| block.number(0xcafeu64),
+        )
+    }
+}
