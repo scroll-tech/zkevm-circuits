c452e5c4ce17b58008237f8316cccb073240e84c
lightsing <light.tsing@gmail.com>
2024-04-26 16:25:09 +0800

    fix doc

--- bus-mapping/src/lib.rs
+++ bus-mapping/src/lib.rs
@@ -49,9 +49,8 @@
 //!
 //! ```rust, no_run
 //! use bus_mapping::{Error, mock::BlockData};
-//! use bus_mapping::state_db::{self, StateDB, CodeDB};
 //! use eth_types::{
-//!     self, address, Address, Word, Hash, U64, GethExecTrace, GethExecStep, geth_types::GethData, bytecode
+//!     self, address, Address, Word, Hash, U64, GethExecTrace, GethExecStep, geth_types::GethData, bytecode, state_db::{self, StateDB, CodeDB}
 //! };
 //! use mock::test_ctx::{TestContext, helpers::*};
 //! use eth_types::evm_types::Gas;
