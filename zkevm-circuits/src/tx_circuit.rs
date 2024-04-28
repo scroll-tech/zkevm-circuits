03aeb5940a5220962ec2f9d7b0dd6f34207c37cd
Ray Gao <qg2153@columbia.edu>
2024-04-28 15:31:50 -0400

    Add precompile transition scenario

--- zkevm-circuits/src/tx_circuit.rs
+++ zkevm-circuits/src/tx_circuit.rs
@@ -304,7 +304,9 @@ impl TxRomTable {
                     (TxFieldTag::AccessListStorageKey, CallData, 0, 1, 0),
                     // Continue padding. Padding has the Calldata tag
                     (CallData, CallData, 1, 1, 0),
+                    // Precompile call transition scenarios
                     (CallData, TxFieldTag::Null, 1, 1, 0),
+                    (CallData, TxFieldTag::Null, 0, 1, 0),
                 ];
 
                 for (offset, scenario) in transition_scenarios.into_iter().enumerate() {
