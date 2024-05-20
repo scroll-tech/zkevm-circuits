7ff8fd4d97a46b748be28b5d96baba0ff7579820
Rohit Narurkar <rohit.narurkar@proton.me>
2024-05-20 09:38:31 +0100

    tbt: test for multi-blob

--- aggregator/src/aggregation/decoder.rs
+++ aggregator/src/aggregation/decoder.rs
@@ -5134,7 +5134,8 @@ mod tests {
     }
 
     #[test]
-    fn test_decoder_config_blob() -> Result<(), std::io::Error> {
+    #[ignore = "single_blob: heavy"]
+    fn test_decoder_config_single_blob() -> Result<(), std::io::Error> {
         let mut blob_files = fs::read_dir("./data/test_blobs")?
             .map(|entry| entry.map(|e| e.path()))
             .collect::<Result<Vec<_>, std::io::Error>>()?;
@@ -5264,6 +5265,77 @@ mod tests {
         let k = 18;
         let decoder_config_tester: DecoderConfigTester<256, 256> =
             DecoderConfigTester { raw, compressed, k };
+        let mock_prover = MockProver::<Fr>::run(k, &decoder_config_tester, vec![]).unwrap();
+        mock_prover.assert_satisfied_par();
+
+        Ok(())
+    }
+
+    #[test]
+    #[ignore = "large_multi_block: heavy"]
+    fn test_decoder_config_large_multi_block() -> Result<(), std::io::Error> {
+        let mut batch_files = fs::read_dir("./data/test_blobs/multi")?
+            .map(|entry| entry.map(|e| e.path()))
+            .collect::<Result<Vec<_>, std::io::Error>>()?;
+        batch_files.sort();
+
+        let mut multi_batch_data = Vec::with_capacity(500_000);
+        for batch_file in batch_files {
+            let batch_data = fs::read(batch_file)?;
+            multi_batch_data.extend_from_slice(&batch_data);
+        }
+
+        let encoded_multi_batch_data = {
+            // compression level = 0 defaults to using level=3, which is zstd's default.
+            let mut encoder =
+                zstd::stream::write::Encoder::new(Vec::new(), 0).expect("Encoder construction");
+
+            // disable compression of literals, i.e. literals will be raw bytes.
+            encoder
+                .set_parameter(zstd::stream::raw::CParameter::LiteralCompressionMode(
+                    zstd::zstd_safe::ParamSwitch::Disable,
+                ))
+                .expect("Encoder set_parameter: LiteralCompressionMode");
+            // set target block size to fit within a single block.
+            encoder
+                .set_parameter(zstd::stream::raw::CParameter::TargetCBlockSize(124 * 1024))
+                .expect("Encoder set_parameter: TargetCBlockSize");
+            // do not include the checksum at the end of the encoded data.
+            encoder
+                .include_checksum(false)
+                .expect("Encoder include_checksum: false");
+            // do not include magic bytes at the start of the frame since we will have a single
+            // frame.
+            encoder
+                .include_magicbytes(false)
+                .expect("Encoder include magicbytes: false");
+            // set source length, which will be reflected in the frame header.
+            encoder
+                .set_pledged_src_size(Some(multi_batch_data.len() as u64))
+                .expect("Encoder src_size: raw.len()");
+            // include the content size to know at decode time the expected size of decoded data.
+            encoder
+                .include_contentsize(true)
+                .expect("Encoder include_contentsize: true");
+
+            encoder
+                .write_all(&multi_batch_data)
+                .expect("Encoder wirte_all");
+            encoder.finish().expect("Encoder success")
+        };
+
+        println!("len(multi_batch_data)={:?}", multi_batch_data.len());
+        println!(
+            "len(encoded_multi_batch_data)={:?}",
+            encoded_multi_batch_data.len()
+        );
+
+        let k = 20;
+        let decoder_config_tester: DecoderConfigTester<1024, 512> = DecoderConfigTester {
+            raw: multi_batch_data,
+            compressed: encoded_multi_batch_data,
+            k,
+        };
         let mock_prover = MockProver::<Fr>::run(k, &decoder_config_tester, vec![]).unwrap();
         mock_prover.assert_satisfied_par();
 
