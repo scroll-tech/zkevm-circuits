359581769463627ab9a71ad99b2f27993b2e8afb
Zhang Zhuo <mycinbrin@gmail.com>
2024-06-07 15:06:47 +0800

    fmt

--- prover/src/lib.rs
+++ prover/src/lib.rs
@@ -23,10 +23,10 @@ pub mod types;
 pub mod utils;
 pub mod zkevm;
 
-pub use zkevm_circuits;
 pub use aggregator::{check_chunk_hashes, BatchHash, MAX_AGG_SNARKS};
 pub use common::{ChunkInfo, CompressionCircuit};
 pub use eth_types::l2_types::BlockTrace;
 pub use proof::{BatchProof, ChunkProof, EvmProof, Proof};
 pub use snark_verifier_sdk::{CircuitExt, Snark};
 pub use types::{BatchProvingTask, ChunkProvingTask, WitnessBlock};
+pub use zkevm_circuits;
