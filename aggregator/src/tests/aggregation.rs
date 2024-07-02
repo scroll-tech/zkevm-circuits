efc469f1586355be848e1374fbac7d03bfdc05df
Ray Gao <qg2153@columbia.edu>
2024-07-02 18:44:42 -0400

    Increase k

--- aggregator/src/tests/aggregation.rs
+++ aggregator/src/tests/aggregation.rs
@@ -17,7 +17,7 @@ use crate::{
 fn test_max_agg_snarks_batch_circuit() {
     // batch_circuit_debug
     // let k = 21;
-    let k = 18;
+    let k = 19;
 
     // This set up requires one round of keccak for chunk's data hash
     let circuit: BatchCircuit<MAX_AGG_SNARKS> = build_new_batch_circuit(2, k);
