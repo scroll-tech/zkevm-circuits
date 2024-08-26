339fe409d62ebb6504ea45cbfb7161fa5cdfb4f3
Rohit Narurkar <rohit.narurkar@proton.me>
2024-08-26 18:11:53 +0100

    add sanity check (decoded blob == batch bytes)

--- prover/src/aggregator/prover.rs
+++ prover/src/aggregator/prover.rs
@@ -1,6 +1,8 @@
 use std::{collections::BTreeMap, env, iter::repeat};
 
-use aggregator::{BatchHash, BatchHeader, ChunkInfo, MAX_AGG_SNARKS};
+use aggregator::{
+    eip4844::decode_blob, BatchData, BatchHash, BatchHeader, ChunkInfo, MAX_AGG_SNARKS,
+};
 use anyhow::{bail, Result};
 use eth_types::H256;
 use halo2_proofs::{halo2curves::bn256::Bn256, poly::kzg::commitment::ParamsKZG};
@@ -212,6 +214,16 @@ impl<'params> Prover<'params> {
         let batch_hash = batch_header.batch_hash();
         let batch_info: BatchHash<N_SNARKS> =
             BatchHash::construct(&chunk_hashes, batch_header, &batch.blob_bytes);
+        let batch_data: BatchData<N_SNARKS> = BatchData::from(&batch_info);
+
+        // sanity check:
+        // - conditionally decoded blob should match batch data.
+        let batch_bytes = batch_data.get_batch_data_bytes();
+        let decoded_blob_bytes = decode_blob(&batch.blob_bytes)?;
+        assert_eq!(
+            batch_bytes, decoded_blob_bytes,
+            "BatchProvingTask(sanity) mismatch batch bytes and decoded blob bytes",
+        );
 
         let layer3_snark = self.prover_impl.load_or_gen_agg_snark(
             name,
