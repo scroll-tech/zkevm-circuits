0615f4f18a5a92ab80021fe7eb3b2c1e4bb7d690
Dream Wu <wwuwwei@126.com>
2024-09-03 16:21:25 +0800

    move test out of tx_access_list

--- zkevm-circuits/src/evm_circuit/execution/begin_tx.rs
+++ zkevm-circuits/src/evm_circuit/execution/begin_tx.rs
@@ -1277,7 +1277,14 @@ mod test {
     use crate::{evm_circuit::test::rand_bytes, test_util::CircuitTestBuilder};
     use bus_mapping::evm::OpcodeId;
     use eth_types::{
-        self, address, bytecode, evm_types::GasCost, word, Address, Bytecode, Hash, Word, U256,
+        self, address, bytecode,
+        evm_types::gas_utils::{tx_access_list_gas_cost, tx_data_gas_cost},
+        evm_types::GasCost,
+        word, Address, Bytecode, Error, Hash, Word, H160, H256, U256, U64,
+    };
+    use ethers_core::types::{
+        transaction::eip2718::TypedTransaction, Eip1559TransactionRequest, NameOrAddress,
+        Signature, Transaction as EthTransaction, TransactionRequest,
     };
     use ethers_core::{types::Bytes, utils::get_contract_address};
     use mock::{eth, gwei, MockTransaction, TestContext, MOCK_ACCOUNTS};
@@ -1757,5 +1764,77 @@ mod test {
         CircuitTestBuilder::new_from_test_ctx(ctx)
             .block_modifier(Box::new(|block| block.circuits_params.max_txs = 3))
             .run();
+    }
+
+    // TODO: move test to approriate place
+    #[test]
+    fn test_legacy_tx_pre_eip255() {
+        let ctx = build_legacy_ctx(gwei(80_000)).unwrap();
+        CircuitTestBuilder::new_from_test_ctx(ctx).run();
+    }
+
+    // build pre-eip155 tx
+    fn build_legacy_ctx(sender_balance: Word) -> Result<TestContext<2, 1>, Error> {
+        TestContext::new(
+            None,
+            |accs| {
+                accs[0].address(MOCK_ACCOUNTS[0]).balance(sender_balance);
+                accs[1].address(MOCK_ACCOUNTS[1]).balance(eth(1));
+            },
+            |mut txs, _accs| {
+                build_pre_eip155_tx();
+            },
+            |block, _tx| block.number(0xcafeu64),
+        )
+    }
+
+    fn build_pre_eip155_tx() -> Transaction {
+        // pre-eip155 tx downloaded from [etherscan](https://etherscan.io/getRawTx?tx=0x9cd2288e69623b109e25edc46bc518156498b521e5c162d96e1ab392ff1d9dff)
+        let eth_tx = TransactionRequest::new()
+            .from(address!("0xcf40d0d2b44f2b66e07cace1372ca42b73cf21a3"))
+            .nonce(word!("0x2ea8"))
+            .gas_price(word!("0x098bca5a00"))
+            .gas(word!("0x0249f0"))
+            .value(word!("0x00"))
+            .data(hex::decode("606060405260008054600160a060020a0319163317905560f2806100236000396000f3606060405260e060020a6000350463f5537ede8114601c575b6002565b3460025760f06004356024356044356000805433600160a060020a039081169116141560ea5783905080600160a060020a031663a9059cbb84846000604051602001526040518360e060020a0281526004018083600160a060020a0316815260200182815260200192505050602060405180830381600087803b1560025760325a03f1156002575050604080518481529051600160a060020a0386811693508716917fd0ed88a3f042c6bbb1e3ea406079b5f2b4b198afccaa535d837f4c63abbc4de6919081900360200190a35b50505050565b00").expect("hex data can be decoded"));
+
+        let eth_sig = Signature {
+            r: word!("0x90b751c5870e9bc071c8d6b2bf1ee80f36ee7efd8e6fbabaa25bd3b8b68cfe9b"),
+            s: word!("0x79c25a01f12493a6d35f1330306d4e3c4e782fcbffc64c6809959577f41ff248"),
+            v: 0x1c,
+        };
+
+        let signed_bytes = eth_tx.rlp_signed(&eth_sig).to_vec();
+        let unsigned_bytes = eth_tx.rlp_unsigned().to_vec();
+
+        let eth_tx_hash = keccak256(&signed_bytes);
+        assert_eq!(
+            hex::encode(eth_tx_hash),
+            "9cd2288e69623b109e25edc46bc518156498b521e5c162d96e1ab392ff1d9dff"
+        );
+
+        let mut tx = Transaction::new_from_rlp_bytes(1, PreEip155, signed_bytes, unsigned_bytes);
+
+        tx.hash = H256(eth_tx_hash);
+        tx.block_number = 1;
+        tx.id = 1;
+        tx.nonce = eth_tx.nonce.unwrap().as_u64();
+        tx.gas_price = eth_tx.gas_price.unwrap();
+        tx.gas = eth_tx.gas.unwrap().as_u64();
+        tx.call_data = eth_tx.data.map_or(vec![], |data| data.to_vec());
+        tx.callee_address = eth_tx.to.as_ref().map(|to| match *to {
+            NameOrAddress::Name(_) => panic!("eth_tx.to must be an address"),
+            NameOrAddress::Address(to) => to,
+        });
+        tx.caller_address = eth_tx.from.unwrap();
+        tx.is_create = eth_tx.to.is_none();
+        tx.call_data_length = tx.call_data.len();
+        tx.call_data_gas_cost = tx_data_gas_cost(&tx.call_data);
+        tx.tx_data_gas_cost = tx_data_gas_cost(&tx.rlp_signed);
+        tx.v = eth_sig.v;
+        tx.r = eth_sig.r;
+        tx.s = eth_sig.s;
+
+        tx
     }
 }
