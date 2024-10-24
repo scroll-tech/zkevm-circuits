cb98f49b9be2d66a26bfeb95217ed85f3e128646
Dream Wu <wwuwwei@126.com>
2024-10-24 17:07:55 +0800

    comment out p256 crate test

--- zkevm-circuits/src/sig_circuit/test.rs
+++ zkevm-circuits/src/sig_circuit/test.rs
@@ -240,140 +240,10 @@ fn p256_sign_verify() {
     use super::utils::LOG_TOTAL_NUM_ROWS;
     use crate::sig_circuit::utils::MAX_NUM_SIG;
     use halo2_proofs::halo2curves::bn256::Fr;
-    use p256::ecdsa::signature::hazmat::{PrehashSigner, PrehashVerifier};
-    use p256::ecdsa::signature::DigestSigner;
-    use p256::ecdsa::{
-        signature::Signer, signature::Verifier, Signature, SigningKey, VerifyingKey,
-    };
-    use p256::{
-        elliptic_curve::generic_array::GenericArray, elliptic_curve::Curve, AffinePoint, NistP256,
-    };
     use rand::SeedableRng;
     use rand_xorshift::XorShiftRng;
     use sha3::{Digest, Keccak256};
-    //use hex_literal::hex;
-    use ff::FromUniformBytes;
-    use ff::PrimeField;
-    use halo2_proofs::arithmetic::CurveAffine;
-    use halo2_proofs::halo2curves::group::GroupEncoding;
-    use halo2_proofs::halo2curves::serde::SerdeObject;
-    use num::{BigUint, Num};
-    use p256::elliptic_curve::point::AffineCoordinates;
-    use p256::pkcs8::EncodePublicKey;
-    use p256::PublicKey;
-
     let mut rng = XorShiftRng::seed_from_u64(10000);
-
-    // p256 crate generate signature
-    // {
-    //     let mut signatures = Vec::new();
-    //     let msg = gen_msg(&mut rng);
-
-    //     let msg_hash_bytes: [u8; 32] = Keccak256::digest(&msg)
-    //         .as_slice()
-    //         .to_vec()
-    //         .try_into()
-    //         .expect("hash length isn't 32 bytes");
-    //     let msg_hash = secp256r1::Fq::from_bytes(&msg_hash_bytes).unwrap();
-    //     let (sk, pk) = gen_key_pair_r1(&mut rng);
-    //     println!("sk_fq raw bytes {:?}", sk.to_raw_bytes());
-    //     println!("sk_fq to_bytes {:?}", sk.to_bytes()); // = to_repr
-
-    //     println!("pk_fp.x {:?}", pk.x.to_bytes());
-
-    //     let (r, s, v) = sign_r1_with_rng(&mut rng, sk, msg_hash);
-    //     // signatures.push(SignData {
-    //     //     signature: (r, s, v),
-    //     //     pk,
-    //     //     msg: msg.into(),
-    //     //     msg_hash,
-    //     // });
-
-    //     let signing_key = SigningKey::from_slice(&sk.to_raw_bytes()).expect("get private from sk");
-    //     println!("signing_key = {:?}", signing_key.to_bytes());
-    //     let verifying_key = signing_key.verifying_key();
-    //     // let vk= verifying_key.to_encoded_point(false);
-    //     println!("verifying_key = {:?}", verifying_key);
-
-    //     let signature: Signature = signing_key
-    //         .sign_prehash(&msg_hash_bytes)
-    //         .expect("sign failed");
-    //     let sig_bytes = signature.to_bytes();
-    //     let mut sig_r_bytes: [u8; 32] = sig_bytes[..32].try_into().expect("failed ");
-    //     let mut sig_s_bytes: [u8; 32] = sig_bytes[32..64].try_into().expect("failed ");
-    //     // let r = secp256r1::Fq::from_bytes_le(&sig_r_bytes);
-    //     // let s = secp256r1::Fq::from_bytes_le(&sig_s_bytes);
-    //     sig_r_bytes.reverse();
-    //     sig_s_bytes.reverse();
-    //     // let r = secp256r1::Fq::from_bytes(&sig_r_bytes).unwrap();
-    //     // let s = secp256r1::Fq::from_bytes(&sig_s_bytes).unwrap();
-    //     // put into sig queue for sig circuit verification
-    //     // hack: custom pk
-    //     fn fe_from_str(string: impl AsRef<str>) -> secp256r1::Fp {
-    //         let string = string.as_ref();
-    //         let oct = if let Some(hex) = string.strip_prefix("0x") {
-    //             BigUint::from_str_radix(hex, 16).unwrap().to_string()
-    //         } else {
-    //             string.to_string()
-    //         };
-
-    //         secp256r1::Fp::from_str_vartime(&oct).unwrap()
-    //     }
-    //     // let x = fe_from_str("0x972eb906f4c9315a6bd0e3cf80de7faf5c7d924bf9db641e952065952bbda42c");
-    //     // let y = fe_from_str("0x548448473cfb708b0e0835a75fdb690a01e2023ea7f3f17c0fb5dea77086996a");
-
-    //     let public_key: PublicKey = verifying_key.into();
-    //     let pubkey_affine = public_key.as_affine();
-    //     let affine_x = pubkey_affine.x();
-
-    //     let mut field_x: [u8; 32] = pubkey_affine.x().into();
-    //     field_x.reverse();
-    //     let x_fp = secp256r1::Fp::from_bytes(&field_x).unwrap();
-    //     println!("x_fp : {:?}", x_fp.to_bytes());
-
-    //     // calculate x^3 + ax + b (mod p)
-    //     let x3 = x_fp.square() * x_fp;
-    //     let ax = secp256r1::Secp256r1Affine::a() * x_fp;
-    //     let y2 = x3 + ax + secp256r1::Secp256r1Affine::b();
-    //     let y = y2.sqrt().unwrap();
-
-    //     //let pk = Secp256r1Affine::from_xy(x_fp, y).unwrap();
-
-    //     signatures.push(SignData {
-    //         signature: (r, s, 0),
-    //         pk,
-    //         msg: msg.into(),
-    //         msg_hash,
-    //     });
-
-    //     println!("recover y: {:?}", y);
-    //     println!("new pk : {:?}", pk);
-    //     let mut r_bytes_1 = r.to_bytes();
-    //     let mut s_bytes_1 = s.to_bytes();
-    //     r_bytes_1.reverse();
-    //     s_bytes_1.reverse();
-
-    //     let signature2 = Signature::from_slice([r_bytes_1, s_bytes_1].concat().as_slice())
-    //         .expect("no error");
-    //     println!(
-    //         "signature {:?} ",
-    //         signature.to_bytes(),
-    //     );
-    //     println!(
-    //         "signature2 {:?}",
-    //         signature2.to_bytes(),
-    //     );
-
-    //     let is_valid = verifying_key
-    //         .verify_prehash(&msg_hash_bytes, &signature2)
-    //         .is_ok();
-    //     println!("Signature is valid: {}", is_valid);
-
-    //     let k = LOG_TOTAL_NUM_ROWS as u32;
-    //     run::<Fr>(k, 1, vec![], signatures);
-
-    //     log::debug!("end of testing for msg_hash = 0");
-    // }
 
     // msg_hash == 0
     {
@@ -382,7 +252,7 @@ fn p256_sign_verify() {
 
         let (sk, pk) = gen_key_pair_r1(&mut rng);
         let msg = gen_msg(&mut rng);
-        let msg_hash = secp256r1::Fq::zero(); // 257 ok, 250 not ok
+        let msg_hash = secp256r1::Fq::zero();
         println!("original pk {:?}", pk);
 
         let (r, s, v) = sign_r1_with_rng(&mut rng, sk, msg_hash);
@@ -403,30 +273,29 @@ fn p256_sign_verify() {
     }
 
     // msg_hash == 1
-    // {
-    //     log::debug!("testing for msg_hash = 1");
-    //     let mut signatures = Vec::new();
+    {
+        log::debug!("testing for msg_hash = 1");
+        let mut signatures = Vec::new();
 
-    //     let (sk, pk) = gen_key_pair_r1(&mut rng);
-    //     let msg = gen_msg(&mut rng);
-    //     let msg_hash = secp256r1::Fq::one();
-    //     let (r, s, v) = sign_r1_with_rng(&mut rng, sk, msg_hash);
-    //     signatures.push(SignData {
-    //         signature: (r, s, v),
-    //         pk,
-    //         msg: msg.into(),
-    //         msg_hash,
-    //     });
+        let (sk, pk) = gen_key_pair_r1(&mut rng);
+        let msg = gen_msg(&mut rng);
+        let msg_hash = secp256r1::Fq::one();
+        let (r, s, v) = sign_r1_with_rng(&mut rng, sk, msg_hash);
+        signatures.push(SignData {
+            signature: (r, s, v),
+            pk,
+            msg: msg.into(),
+            msg_hash,
+        });
 
-    //     let k = LOG_TOTAL_NUM_ROWS as u32;
-    //     run::<Fr>(k, 1, vec![], signatures);
+        let k = LOG_TOTAL_NUM_ROWS as u32;
+        run::<Fr>(k, 1, vec![], signatures);
 
-    //     log::debug!("end of testing for msg_hash = 1");
-    // }
+        log::debug!("end of testing for msg_hash = 1");
+    }
 
     // random msg_hash
-    // let max_sigs = [1, 16, MAX_NUM_SIG]; // enable this line after testing
-    let max_sigs = [1];
+    let max_sigs = [1, 16, MAX_NUM_SIG];
 
     for max_sig in max_sigs.iter() {
         log::debug!("testing for {} signatures", max_sig);
@@ -440,7 +309,7 @@ fn p256_sign_verify() {
                 .try_into()
                 .expect("hash length isn't 32 bytes");
             let msg_hash = secp256r1::Fq::from_bytes(&msg_hash).unwrap();
-            //let msg_hash = secp256r1::Fq::one();
+
             let (r, s, v) = sign_r1_with_rng(&mut rng, sk, msg_hash);
             signatures.push(SignData {
                 signature: (r, s, v),
@@ -456,6 +325,106 @@ fn p256_sign_verify() {
         log::debug!("end of testing for {} signatures", max_sig);
     }
 }
+
+// TODO: this test fail due to signature/pubkey representation(sec1 encoding/compress etc)
+// need to decode correct bytes and re-construct signature/pubkey for testing
+// test rust p256 crate apis: sign & verify
+// #[test]
+// fn p256_crate_sig_verify() {
+//     use super::utils::LOG_TOTAL_NUM_ROWS;
+//     use crate::sig_circuit::utils::MAX_NUM_SIG;
+//     use halo2_proofs::halo2curves::bn256::Fr;
+//     use p256::ecdsa::signature::hazmat::{PrehashSigner, PrehashVerifier};
+//     use p256::ecdsa::{
+//         signature::Signer, signature::Verifier, Signature, SigningKey, VerifyingKey,
+//     };
+//     use rand::SeedableRng;
+//     use rand_xorshift::XorShiftRng;
+//     use sha3::{Digest, Keccak256};
+//     use ff::FromUniformBytes;
+//     use ff::PrimeField;
+//     use halo2_proofs::arithmetic::CurveAffine;
+//     use halo2_proofs::halo2curves::group::GroupEncoding;
+//     use halo2_proofs::halo2curves::serde::SerdeObject;
+//     use p256::elliptic_curve::point::AffineCoordinates;
+//     use p256::pkcs8::EncodePublicKey;
+//     use p256::PublicKey;
+
+//     let mut rng = XorShiftRng::seed_from_u64(10000);
+
+//     // p256 crate generate signature
+//     {
+//         let mut signatures = Vec::new();
+//         let msg = gen_msg(&mut rng);
+
+//         let msg_hash_bytes: [u8; 32] = Keccak256::digest(&msg)
+//             .as_slice()
+//             .to_vec()
+//             .try_into()
+//             .expect("hash length isn't 32 bytes");
+//         let msg_hash = secp256r1::Fq::from_bytes(&msg_hash_bytes).unwrap();
+//         let (sk, pk) = gen_key_pair_r1(&mut rng);
+//         println!("sk_fq raw bytes {:?}", sk.to_raw_bytes());
+
+//         let signing_key = SigningKey::from_slice(&sk.to_raw_bytes()).expect("get private from sk");
+//         println!("signing_key = {:?}", signing_key.to_bytes());
+//         let verifying_key = signing_key.verifying_key();
+//         // let vk= verifying_key.to_encoded_point(false);
+//         println!("verifying_key = {:?}", verifying_key);
+
+//         let signature: Signature = signing_key
+//             .sign_prehash(&msg_hash_bytes)
+//             .expect("sign failed");
+//         let sig_bytes = signature.to_bytes();
+//         let mut sig_r_bytes: [u8; 32] = sig_bytes[..32].try_into().expect("failed ");
+//         let mut sig_s_bytes: [u8; 32] = sig_bytes[32..64].try_into().expect("failed ");
+//         let r = secp256r1::Fq::from_raw_bytes(&sig_r_bytes).unwrap();
+//         let s = secp256r1::Fq::from_raw_bytes(&sig_s_bytes).unwrap();
+//         // hack: custom pk
+
+//         let public_key: PublicKey = verifying_key.into();
+//         let pubkey_affine = public_key.as_affine();
+//         let affine_x = pubkey_affine.x();
+
+//         let mut field_x: [u8; 32] = pubkey_affine.x().into();
+//         let x_fp = secp256r1::Fp::from_raw_bytes_unchecked(&field_x);
+//         // println!("x_fp : {:?}", x_fp.to_bytes());
+//         // can not get y from pubkey_affine direclty.
+//         // calculate x^3 + ax + b (mod p)
+//         let x3 = x_fp.square() * x_fp;
+//         let ax = secp256r1::Secp256r1Affine::a() * x_fp;
+//         let y2 = x3 + ax + secp256r1::Secp256r1Affine::b();
+//         // check if need negative form
+//         let y = y2.sqrt().unwrap();
+
+//         let pk = Secp256r1Affine::from_xy(x_fp, y).unwrap();
+
+//         signatures.push(SignData {
+//             signature: (r, s, 0),
+//             pk,
+//             msg: msg.into(),
+//             msg_hash,
+//         });
+
+//         let mut r_bytes_1 = r.to_bytes();
+//         let mut s_bytes_1 = s.to_bytes();
+//         r_bytes_1.reverse();
+//         s_bytes_1.reverse();
+
+//         let signature2 = Signature::from_slice([r_bytes_1, s_bytes_1].concat().as_slice())
+//             .expect("no error");
+
+//         let is_valid = verifying_key
+//             .verify_prehash(&msg_hash_bytes, &signature2)
+//             .is_ok();
+//         println!("Signature is valid: {}", is_valid);
+
+//         let k = LOG_TOTAL_NUM_ROWS as u32;
+//         run::<Fr>(k, 1, vec![], signatures);
+
+//         log::debug!("end of testing for msg_hash = 0");
+//     }
+// }
 
 // test for both secp256k1 and secp256r1 signatures
 #[test]
