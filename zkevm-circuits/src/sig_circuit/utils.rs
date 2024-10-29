a045cda4191fae7c1e74a1787a2166ea4472ff0c
Zhang Zhuo <mycinbrin@gmail.com>
2024-10-29 09:33:46 +0800

    Update utils.rs

--- zkevm-circuits/src/sig_circuit/utils.rs
+++ zkevm-circuits/src/sig_circuit/utils.rs
@@ -13,7 +13,7 @@ use halo2_proofs::{
 
 // Hard coded parameters.
 // FIXME: allow for a configurable param.
-pub(super) const MAX_NUM_SIG_K1: usize = 110;
+pub(super) const MAX_NUM_SIG_K1: usize = 50;
 // MAX NUM OF SIG_R1
 pub(super) const MAX_NUM_SIG_R1: usize = 15;
 
